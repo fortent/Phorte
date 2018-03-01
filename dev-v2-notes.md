1ac5159cc757933c29d485a7c3aeade8887a699e
fortent <natasha.tavaresforte@gmail.com>
2018-03-01 20:59:08 +0000

    added log4j2

---
+++ dev-v2-notes.md
@@ -0,0 +1,5 @@
+## DEV-v2 TO DO notes
+
+A set of notes for work to do in this branch.
+
+Also a reminder of the branch you are in.
--- src/main/java/com/NatashaForte/Phorte/App.java
+++ src/main/java/com/NatashaForte/Phorte/App.java
@@ -3,61 +3,105 @@ package com.NatashaForte.Phorte;
 import java.util.Date;
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-/**
- * Hello world!
- *
- */
 
 public class App 
 {
     public static void main( String[] args )
-    
     {
-        seeCommandlineInput(args);
-        
-        actionCommandlineInput(args);
-        
-        
+		// To view the arguments being entered
+		seeCommandlineInput(args);
+			
+		// To instantiate App class based in the parameters entered at the commandline
+		actionCommandlineInput(args);
     }
     
-    private Scanner someInput;
-    private Date today;
+	// DATA
+	//............................................................
+	//define attributes
+	
+	private	Scanner someInput;
+	private Date today;
+	
+	// This is added to every class that needs to log with one change
+	// The getLogger( ) part should contain the name of the class its in
+	private static Logger LOG;
+	
+	// CONSTRUCTORS
+	//............................................................
+	
+	public App()
+	{		
+		//associate logging with this class so know the messages that came from objects of this class
+		LOG = LogManager.getLogger(App.class);
+		
+		//test the logging
+		testLogOutput();
+		
+		this.someInput = new Scanner(System.in);
+		
+		//do something here
+		System.out.println(" \n Soon ... stuff will happen here");		
+		
+		//pause before exit (this is only useful if an error occurs)
+        System.out.println(" \n Press enter to exit the program");
+		this.someInput.nextLine();
+
+		//close the program without error
+		System.exit(0);
+	}
     
-    public App()
     
-    {
-    		this.someInput = new Scanner(System.in);
-    		
-    		System.out.println("\n Soon stuff will happen here");
-    		
-    		System.out.println("\n press enter to exit the program");
-    		
-    		this.someInput.nextLine();
-    		
-    		System.exit(0);
-    }
+	// METHODS used by main() or debug methods - note they are static methods
+	//............................................................
+	
+	/**
+	 * action the arguments presented at the command line
+	 * instantiate the App class based on the arguments passed
+	 */
+	 private static void actionCommandlineInput( String args[] )
+	 {
+		 // no special instantiation yet as don't pass args to it
+		 App anApp = new App();
+	 }
     
-    private static void actionCommandlineInput( String args[])
-    {
-    		App anApp = new App();
-    }
+	/**
+	 * View the arguments presented at the commandline
+	 * This is for debug and demo purposes
+	 */
+	 private static void seeCommandlineInput( String args[] )
+	 {	
+		if (args.length == 0)
+        {
+            System.out.println("There were no commandline arguments passed!");
+        }
+		else
+		{
+			// display the command line  entered 
+			for(int i = 0; i < args.length; i++) 
+			{
+	            System.out.println(args[i]);
+	            
+	        }
+		}
+		 
+	 }//EOM
+	 
+	/**
+	 * Test the Log4J2 logging
+	 */
+	 private static void testLogOutput()
+	 {
+		LOG.debug("Log test: Test printed on debug");
+	    LOG.info("Log test: Test printed on info");
+	    LOG.warn("Log test: Test printed on warn");
+	    LOG.error("Log test: Test printed on error");
+	    LOG.fatal("Log test: Test printed on fatal");
+	
+	    LOG.info("Appending string: {}.", "Application log test message - Hi");
+		 
+	 }//EOM
     
-    private static void seeCommandlineInput( String args[])
-    {
-    		if (args.length == 0)
-    		{
-    			System.out.println("There were no commandline arguments passed!");
-    		}
-    		
-    		else
-    		{
-    			for(int i=0; i < args.length; i++)
-    			{
-    				System.out.println(args[1]);
-    				
-    			}
- 
-    		}
-    }
-}
+}//EOC
