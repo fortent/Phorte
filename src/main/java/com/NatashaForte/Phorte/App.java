c8af11a137326390956c5c9cdc191625dfdd6186
fortent <natasha.tavaresforte@gmail.com>
2018-03-01 12:48:41 +0000

    App modified from default.

--- src/main/java/com/NatashaForte/Phorte/App.java
+++ src/main/java/com/NatashaForte/Phorte/App.java
@@ -1,13 +1,63 @@
 package com.NatashaForte.Phorte;
 
+import java.util.Date;
+import java.util.Scanner;
+
+
 /**
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
+    
     {
-        System.out.println( "Hello World!" );
+        seeCommandlineInput(args);
+        
+        actionCommandlineInput(args);
+        
+        
+    }
+    
+    private Scanner someInput;
+    private Date today;
+    
+    public App()
+    
+    {
+    		this.someInput = new Scanner(System.in);
+    		
+    		System.out.println("\n Soon stuff will happen here");
+    		
+    		System.out.println("\n press enter to exit the program");
+    		
+    		this.someInput.nextLine();
+    		
+    		System.exit(0);
+    }
+    
+    private static void actionCommandlineInput( String args[])
+    {
+    		App anApp = new App();
+    }
+    
+    private static void seeCommandlineInput( String args[])
+    {
+    		if (args.length == 0)
+    		{
+    			System.out.println("There were no commandline arguments passed!");
+    		}
+    		
+    		else
+    		{
+    			for(int i=0; i < args.length; i++)
+    			{
+    				System.out.println(args[1]);
+    				
+    			}
+ 
+    		}
     }
 }
